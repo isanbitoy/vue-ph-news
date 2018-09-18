65b5025380f613937c159406b05f2390635b788a
isan <38833845+isanbitoy@users.noreply.github.com>
2018-09-18 13:01:31 +0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
 <template>
   <main id="app-wrapper">
-    <div class="top-banner">
-      <div>
-        <div><flag iso="ph" /></div>
-        <div class="banner-item" style="padding:5px">
-          <span style="color:blue">P</span><span style="color:red">H</span>
-          <span>News Portal</span>
-          <h2>All fresh headlines from Philippines</h2>
-        </div>
-      </div>
-    </div>
+    <news-banner />
     <nav class="sticky-navbar">
       <a v-on:click="getPosts('business')"><span class="item">Business</span></a>
       <a v-on:click="getPosts('entertainment')"><span class="item">Entertainment</span></a>
@@ -21,7 +12,7 @@
 
       <section class="grid-container">
         <news-headline class="big"></news-headline>
-        
+
         <news-top-story></news-top-story>
 
       </section>
@@ -32,18 +23,6 @@
   -->
   </main>
 </template>
-
-<script>
-import NewsHeadline from './components/NewsHeadline.vue'
-import NewsTopStories from './components/NewsTopStories.vue'
-
-export default {
-  components: {
-    NewsHeadline,
-    NewsTopStories
-  }
-}
-</script>
 
 <style>
 html {
@@ -58,31 +37,6 @@ html {
     box-sizing: content-box;
     padding: 0;
     margin: 0;
-}
-.top-banner {
-    width: 100%;
-    height: auto;
-    top: 0;
-    left: 0;
-    background-color: #fff;
-}
-.top-banner > div {
-    display: flex;
-    padding: 1.5em 10%;
-}
-.flag-icon-ph {
-    font-size: 85px;
-    border-radius: 4px;
-    box-sizing: content-box;
-    background-size: contain;
-    background-position: 50%;
-    background-repeat: no-repeat;
-    border: 1px solid #ededed;
-}
-.banner-item span {
-    font-size: 42px;
-    font-weight: bold;
-    letter-spacing: -2px;
 }
 
 /*sticky navigaton bar*/
