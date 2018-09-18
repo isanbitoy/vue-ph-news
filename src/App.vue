10667cd4c1ddac7643c03c5d201a4b49ddac4033
isan <38833845+isanbitoy@users.noreply.github.com>
2018-09-19 00:23:31 +0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -35,9 +35,12 @@
         </flickity>
       </div>
 
-      <news-top-story>
-          
-      </news-top-story>
+      <div>
+        <article v-for="(art, index) in articleContent" :key="index">
+          <div>{{ art.title }}</div>
+        </article>
+      </div>
+      
     </section>
   <!--
     <transition-group name="grid-container" class="grid-container" tag="section">
@@ -49,7 +52,63 @@
 
 <script>
 import axios from 'axios'
-import 
+import Flickity from 'vue-flickity'
+
+const BaseUrl = 'https://newsapi.org/v2/top-headlines?country=ph'
+const ApiKey = '643d0a34867c44cc9519671ec2e0dfbd'
+
+/* build the website url*/
+function buildUrl() {
+  return BaseUrl + "&apiKey=" + ApiKey
+}
+
+function buildUrl2(category) {
+  return BaseUrl + "&category=" + category + "&apiKey=" + ApiKey
+}
+
+export default {
+  components: {
+    Flickity
+  },
+  data: function() {
+    return {
+      articleContent: [],
+      placeholder: 'http://placehold.it/640x480?text=N/A',
+      flickityOptions: {
+        initialIndex: null,
+        prevNextButtons: true,
+        wrapAround: true,
+        pageDots: true,
+        resize: true,
+        contain: true,
+        draggable: false,
+        freeScroll: false,
+        autoPlay: 3000
+      }
+    }
+  },
+  mounted() {
+    this.getPosts()
+  },
+  methods: {
+    getPosts: function() {
+      let url = buildUrl();
+      axios.get(url)
+      .then((response) => {
+        this.articleContent = response.data.articles;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    },
+    next: function() {
+      this.$refs.flickity.next();
+    },
+    previous: function() {
+      this.$refs.flickity.previous();
+    }
+  }
+}
 </script>
 
 <style>
@@ -58,7 +117,7 @@ html {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
-    background-color: #e9e9e9;
+    background-color: #f8f8f8;
 }
 *, *::before, *::after {
     box-sizing: content-box;
@@ -68,7 +127,10 @@ html {
 
 /*sticky navigaton bar*/
 .sticky-navbar {
+    position: -webkit-sticky;
     position: sticky;
+    top: 0;
+    z-index: 1;
     background-color: #fff;
     text-align: center;
     margin: 0px 0px 25px 0px;
@@ -110,8 +172,9 @@ html {
     grid-gap: 2em;
     grid-template-columns: repeat(auto-fit, minmax(320px, auto));
     grid-auto-rows: repeat(auto-fit, minmax(auto, 1fr));
-    justify-content: center;
+    /*justify-content: center;*/
     grid-auto-flow: dense;
+    margin: 0 10%;
 }
 .grid-container .big {
     min-width: 640px;
@@ -122,4 +185,52 @@ html {
     grid-row: span 2;
     background-color: #ededed;
 }
+
+.news-headline-container {
+    grid-column: span 2;
+    max-width: 640px;
+    max-height: 480px;
+}
+.article-container {
+    display: inline-flex;
+    width: 100%;
+    height: auto;
+    background-color: #ededed;
+    box-sizing: border-box;
+}
+.figure-container {
+    position: relative;
+    width: 640px;
+    height: 420px;
+}
+.figure-container img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+.figure-container .overlay-title {
+    position: absolute;
+    background: rgba(0,0,0,.30); 
+    top: 0; left: 0;
+    width: 100%;
+    height: 13%;
+}
+.figure-container .overlay-title h3 {
+    color: #f2f2f2;
+    padding: 5px;
+}
+.figure-container .overlay-source {
+    position: absolute;
+    color: #fff;
+    padding: 5px;
+    bottom: 0; right: 0;
+}
+.content-container {
+    height: 60px;
+    color: #2c3e50;
+}
+
+
 </style>
