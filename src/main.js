935eb96a2e786a7fb001915893b8e0cae58ecfa6
isan <38833845+isanbitoy@users.noreply.github.com>
2018-09-19 23:41:27 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -3,14 +3,10 @@ import App from './App.vue'
 import './registerServiceWorker'
 import FlagIcon from 'vue-flag-icon'
 import NewsBanner from './components/NewsBanner.vue'
-import NewsHeadline from './components/NewsHeadline.vue'
-import NewsTopStories from './components/NewsTopStories.vue'
 
 Vue.config.productionTip = false
 Vue.use(FlagIcon);
 Vue.component('news-banner', NewsBanner);
-Vue.component('news-headline', NewsHeadline);
-Vue.component('news-top-story', NewsTopStories);
 
 new Vue({
   render: h => h(App)
