b5ea654631fc7e86bbfe3b8443610387ee35fd1c
isan <38833845+isanbitoy@users.noreply.github.com>
2018-09-21 21:22:52 +0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -10,65 +10,11 @@
       <a v-on:click="getArticle('technology')"><span class="item">Technology</span></a>
     </nav>
 
-    <!--
-    <section class="grid-container">
-      <div class="news-headline-wrapper">
-        <flickity ref="flickity" 
-                  v-if="Object.keys(headlineContent).length > 0" 
-                  v-bind:options="flickityOptions">
-
-        <article class="headline-article" 
-                 v-for="(headline, index) in headlineContent" 
-                 v-bind:key="index">
-          <a v-bind:title="headline.title" 
-             v-bind:href="headline.url" 
-             target="_blank" 
-             style="text-decoration:none">
-            <figure class="figure-container">
-              <img v-bind:src="headline.urlToImage ? headline.urlToImage : placeholder" />
-              <figcaption>
-                <div class="overlay-title"><h3>{{ headline.title }}</h3></div>
-                <span class="overlay-source">source:&nbsp;{{ headline.source.name }}</span>
-              </figcaption>
-            </figure>
-            <div class="content-container">{{ headline.description | truncate(120) }}</div>
-          </a>
-        </article>
-
-        </flickity>
-      </div>
-
-      <div class="top-story-wrapper">
-        <h3>Top Stories</h3>
-        <article class="top-story-article"
-                 v-for="(topStory, index) in headlineContent.slice(0, 6)" 
-                 v-bind:key="index">
-          <a v-bind:title="topStory.title" 
-             v-bind:href="topStory.url" 
-             target="_blank" 
-             style="text-decoration:none">
-            <div>{{ topStory.title }}</div>
-          </a>
-        </article>
-      </div>
-
-      <div class="main-article-wrapper">
-        <article class="main-article"
-                 v-for="(main, index) in mainContent"
-                 v-bind:key="index">
-          <div>{{ main.title }}</div>
-          
-        </article>
-      </div>
-      
-    </section>
-  -->
-
   <div class="grid-layout">
-
+    <!-- start of flex layout -->
     <div class="flex-layout">
-      <!-- 1 -->
-      <section class="news-headline-wrapper">
+      <!-- first flex container -->
+      <section class="flex-container news-headline">
         <flickity ref="flickity" 
                   v-if="Object.keys(headlineContent).length > 0" 
                   v-bind:options="flickityOptions">
@@ -91,9 +37,8 @@
           </article>
         </flickity>
       </section>
-
-      <!-- 2 -->
-      <section class="top-story-wrapper">
+      <!-- second flex container -->
+      <section class="flex-container top-story">
         <h3>Top Stories</h3>
         <article class="top-story-article"
                  v-for="(topStory, index) in headlineContent.slice(0, 6)" 
@@ -107,12 +52,20 @@
         </article>
       </section>
     </div>
-
-    <div style="background-color:red">
-      me
-    </div>
-    <div style="background-color:red">
-      me
+    <!-- end of flex layout -->
+    <div class="main-article-wrapper">
+      <article class="main-article"
+               v-for="(main, index) in mainContent"
+               v-bind:key="index">
+        <a v-bind:title="main.title"
+           v-bind:href="main.url"
+           target="_blank"
+           style="text-decoration:none">
+          <figure class="">
+            
+          </figure>
+        </a>
+      </article>
     </div>
   </div>
 
@@ -131,27 +84,6 @@ html {
     box-sizing: content-box;
     padding: 0;
     margin: 0;
-}
-
-.grid-layout {
-    display: grid;
-    grid-gap: 2em;
-    grid-template-columns: repeat(auto-fit, minmax(320px, auto));
-    grid-auto-rows: repeat(auto-fit, minmax(auto, 1fr));
-    margin: 0 10%;
-}
-.flex-layout {
-    grid-column: span 3;
-    display: flex;
-}
-.news-headline-wrapper {
-    flex: 2;
-    max-width: 38em;
-    max-height: 30em;
-}
-
-.top-story-wrapper {
-    flex: 1;
 }
 
 /*sticky navigaton bar*/
@@ -195,24 +127,30 @@ html {
     transform-origin: bottom center;
 }
 
-/*grid container*/
-.grid-container {
+/*grid layout*/
+.grid-layout {
     display: grid;
     grid-gap: 2em;
     grid-template-columns: repeat(auto-fit, minmax(320px, auto));
     grid-auto-rows: repeat(auto-fit, minmax(auto, 1fr));
-    /*justify-content: center;*/
-    grid-auto-flow: dense;
     margin: 0 10%;
 }
-
-/*
-.news-headline-wrapper {
-    grid-column: span 2;
-
-    max-width: 42em;
-    max-height: 34em;
-}*/
+.flex-layout {
+    grid-column: span 3;
+    display: flex;
+}
+.flex-container {
+    
+}
+.news-headline {
+    flex: 2;
+    max-width: 38em;
+    max-height: 30em;
+}
+.top-story {
+    flex: 1;
+    background-color: #ededed;
+}
 .headline-article {
     display: inline-flex;
     width: 100%;
@@ -255,9 +193,7 @@ html {
     padding: 5px;
 }
 
-.top-story-wrapper {
-  background-color: #ededed;
-}
+
 .top-story-article {
     margin: 0 0 15px 0;
     border-bottom: 1px solid #111;
