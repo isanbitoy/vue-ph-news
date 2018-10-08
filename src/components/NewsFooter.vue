99ab3237e92d832a952964c949969c7163420616
isan <38833845+isanbitoy@users.noreply.github.com>
2018-10-08 20:59:30 +0800

    Add files via upload

--- src/components/NewsFooter.vue
+++ src/components/NewsFooter.vue
@@ -1,62 +1,52 @@
 <template>
-  <main id="news-footer-wrapper" class="news-footer">
-  	<div class="footer-container">
-  	  <div class="nav-list">
-  	  	<div><a v-on:click="getArticle('business')">Business</a></div>
-  	  	<div><a v-on:click="getArticle('entertainment')">Entertainment</a></div>
-  	  	<div><a v-on:click="getArticle('health')">Health</a></div>
-  	  	<div><a v-on:click="getArticle('science')">Science</a></div>
-  	  	<div><a href="">Sports</a></div>
-  	  	<div><a href="">Technology</a></div>
-  	  </div>
-  	  <div class="footer-item">
-  	  	&copy;&nbsp;{{ year }}
-  	  </div>
+  <footer class="news-footer-wrapper">
+    <ul class="nav-list">
+      <li><a v-on:click="setCategory('business')">Business</a></li>
+      <li><a v-on:click="setCategory('entertainment')">Entertainment</a></li>
+      <li><a v-on:click="setCategory('health')">Health</a></li>
+      <li><a v-on:click="setCategory('science')">Science</a></li>
+      <li><a v-on:click="setCategory('sports')">Sports</a></li>
+      <li><a v-on:click="setCategory('technology')">Technology</a></li>
+    </ul>
+  	<div class="footer-item">
+  	  &copy;<span>&nbsp;</span>{{ year }}
   	</div>
-  </main>
+  </footer>
 </template>
 
-<script>
-import EventBus from './main.js'
-
-export default {
-	methods: {
-		getArticle: function(data) {
-			this.$root.$emit('emitted', data);
-		}
-	}
-}
-</script>
-
 <style scoped>
-.news-footer {
+.news-footer-wrapper {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
   width: 100%;
   height: auto;
-  bottom: 0;
-  left: 0;
-  background: #222;
-}
-.footer-container {
-  display: flex;
+  bottom: 0; left: 0;
   margin: 0 auto;
   padding: 1.5em 3em;
   color: #fff;
+  background: #222;
 }
 .nav-list {
   flex: 1;
   display: block;
+}
+.nav-list li {
+  list-style: none;
   line-height: 1.5em;
 }
-.nav-list a {
+.nav-list li a {
+  cursor: pointer;
   text-decoration: none;
-  color: #fff;
 }
-.nav-list a:hover {
-  text-decoration: underline;
+.nav-list li a:hover {
   color: #a6a6a6;
+  text-decoration: underline;
 }
+
 .footer-item {
   flex: 1;
+  top: 0; right: 0;
 }
 
 </style>
@@ -67,6 +57,12 @@ export default {
 		return {
 			year: new Date().getFullYear()
 		}
+	},
+	methods: {
+		setCategory: function(category) {
+			this.$store.dispatch('LOAD_ARTICLE_NEWS', category)
+      this.$store.commit('SET_CATEGORY', category)
+		}
 	}
-}
+};
 </script>
